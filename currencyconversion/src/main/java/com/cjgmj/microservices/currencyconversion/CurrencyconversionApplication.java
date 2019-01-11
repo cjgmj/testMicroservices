1b25331424786ea2373c2c521ecfcb8f936815d1
cjgmj <cjgmj@hotmail.com>
2019-01-11 21:40:48 +0100

    Microservice using Feign

---
+++ currencyconversion/src/main/java/com/cjgmj/microservices/currencyconversion/CurrencyconversionApplication.java
@@ -0,0 +1,17 @@
+package com.cjgmj.microservices.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.cjgmj.microservices.currencyconversion")
+@EnableDiscoveryClient
+public class CurrencyconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconversionApplication.class, args);
+	}
+
+}
